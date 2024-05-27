ece56097857e074c2f8e0d00bb871da737b5b983
unknown <sundawarifky@gmail.com>
2024-05-27 20:43:08 +0700

    bye

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card" style="border-radius: 15px;">
+                    <div class="card-header">
+                        <span style="color:grey;">HELLO WORLD</span>
+                    </div>
+
+                    <div class="card-body">
+                        <h1 class="btn btn-danger">HALLO</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
